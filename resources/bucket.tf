d1c3b897d493ca39f4accabaf18502efa6d65421
Matheus Vasconcellos <matheusvclls97@gmail.com>
2025-09-27 16:55:50 -0300

    feat: create the raw bucket

--- resources/bucket.tf
+++ resources/bucket.tf
@@ -1,5 +1,6 @@
 variable "bucket_name"       { default = "landing-zone-622813843927" }
 variable "bucket_name_processed"       { default = "processed-zone-622813843927" }
+variable "bucket_name_raw"       { default = "raw-zone-622813843927" }
 variable "bucket_name_queryresults"       { default = "queryresults-zone-622813843927" }
 
 resource "aws_s3_bucket" "dst" {
@@ -12,6 +13,10 @@ resource "aws_s3_bucket" "processed" {
 
 resource "aws_s3_bucket" "queryresults" {
   bucket = var.bucket_name_queryresults
+}
+
+resource "aws_s3_bucket" "raw" {
+  bucket = var.bucket_name_raw
 }
 
 resource "aws_s3_bucket_public_access_block" "dst" {
@@ -32,6 +37,14 @@ resource "aws_s3_bucket_public_access_block" "processed" {
 
 resource "aws_s3_bucket_public_access_block" "queryresults" {
   bucket                  = aws_s3_bucket.queryresults.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "raw" {
+  bucket                  = aws_s3_bucket.raw.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
