b82e70e977005e4315a272b80c7a6b3e0716211c
Matheus Vasconcellos <matheusvclls97@gmail.com>
2025-09-27 13:53:29 -0300

    feat: create the s3 buckets for processed and queryresults

--- resources/bucket.tf
+++ resources/bucket.tf
@@ -1,11 +1,37 @@
 variable "bucket_name"       { default = "landing-zone-622813843927" }
+variable "bucket_name_processed"       { default = "processed-zone-622813843927" }
+variable "bucket_name_queryresults"       { default = "queryresults-zone-622813843927" }
 
 resource "aws_s3_bucket" "dst" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket" "processed" {
+  bucket = var.bucket_name_processed
+}
+
+resource "aws_s3_bucket" "queryresults" {
+  bucket = var.bucket_name_queryresults
+}
+
 resource "aws_s3_bucket_public_access_block" "dst" {
   bucket                  = aws_s3_bucket.dst.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "processed" {
+  bucket                  = aws_s3_bucket.processed.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "queryresults" {
+  bucket                  = aws_s3_bucket.queryresults.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
